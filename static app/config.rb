4c1f6c3bcccf03821f8e7a8c159685042f17627d
Marc Charton <mcharton@altirc.com>
2015-03-06 15:11:09 +0100

    Ajout de compass

---
+++ static app/config.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'styles/'
+sass_dir = 'styles/'
+images_dir = 'images/'
+javascripts_dir = 'javascripts/'
+relative_assets = true
+line_comments = true
